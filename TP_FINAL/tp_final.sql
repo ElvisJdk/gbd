c3bc9ff970d49356176974e52a2f7f0546830cf4
ElvisJdk <149529576+ElvisJdk@users.noreply.github.com>
2026-07-16 13:19:28 -0300

    Add files via upload

--- TP_FINAL/tp_final.sql
+++ TP_FINAL/tp_final.sql
@@ -75,7 +75,7 @@ CREATE TABLE alumnos (
     fecha_nacimiento DATE
 );
 
-CREATE TABLE cursos (
+	CREATE TABLE cursos (
     id_curso INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     profesor VARCHAR(100)
@@ -108,7 +108,162 @@ INSERT INTO inscripciones VALUES
 (3, 1, 7.5),
 (1, 2, 10.0),
 (2, 2, 8.0);
-truncate cursos;
+
+
 
 select * from alumnos;
 select * from cursos;
+use ejercicio_3;
+
+delimiter //
+
+create procedure obtener_promedio(in p_id int)
+begin
+select avg(calificacion) as promedio
+from inscripciones 
+where id_alumno = p_id;
+
+end //
+
+delimiter ;	
+
+CALL obtener_promedio(1);
+
+
+/*
+4 - Crear un procedimiento almacenado "actualizar_stock" que tome como parámetros de entrada el código del producto y 
+	la cantidad a agregar al stock actual. El procedimiento debe actualizar el stock sumando la cantidad especificada
+    al stock actual del producto correspondiente. Verificar mediante ejecución del procedimiento.
+*/
+
+create database ejercicio_4;
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE productos (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  descripcion VARCHAR(255) DEFAULT '',
+  precio DECIMAL(10,2) NOT NULL,
+  stock INT DEFAULT 0
+);
+
+
+CREATE TABLE ventas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    producto_id INT NOT NULL,
+    cantidad INT NOT NULL,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
+
+
+--  Insercion de registros
+INSERT INTO clientes (nombre, direccion, telefono) VALUES
+('Juan Pérez', 'Calle Falsa 123', '555-1234'),
+('María García', 'Avenida Siempreviva 742', '555-5678'),
+('Pedro González', 'Calle 13 No. 6-11', '555-9101'),
+('Ana Hernández', 'Carrera 7 No. 32-60', '555-1212'),
+('Luisa Rodríguez', 'Avenida Boyacá No. 64C-31', '555-1415'),
+('Carlos Vargas', 'Carrera 15 No. 93-75', '555-1617'),
+('Cristina Gómez', 'Carrera 45 No. 34-87', '555-1819'),
+('Javier Torres', 'Calle 45 No. 23-09', '555-2022'),
+('Laura Sánchez', 'Avenida 68 No. 56-18', '555-2225'),
+('Andrés Díaz', 'Carrera 7 No. 11-07', '555-2428');
+
+
+INSERT INTO productos (nombre, descripcion, precio, stock)
+VALUES ('Laptop', 'Laptop HP 15", 8GB RAM, 1TB HDD', 1500.00, 10),
+('Smartphone', 'Smartphone Samsung Galaxy S21', 1000.00, 15),
+('Tablet', 'Tablet Apple iPad Pro 12.9"', 1200.00, 5),
+('Monitor', 'Monitor LG 27", 1440p', 500.00, 20),
+('Teclado', 'Teclado mecánico Logitech G513', 100.00, 30),
+('Mouse', 'Mouse inalámbrico Logitech M720', 50.00, 25),
+('Auriculares', 'Auriculares Sony WH-1000XM4', 300.00, 10),
+('Altavoces', 'Altavoces Bose SoundLink Revolve+', 250.00, 8),
+('Cámara', 'Cámara Canon EOS R5', 4000.00, 2),
+('Impresora', 'Impresora multifunción HP LaserJet Pro M428fdw', 600.00, 5);
+
+
+INSERT INTO ventas (cliente_id, producto_id, cantidad, fecha) VALUES
+(1, 1, 5, '2022-01-01'),
+(1, 2, 3, '2022-01-02'),
+(2, 3, 2, '2022-01-03'),
+(2, 1, 1, '2022-01-04'),
+(3, 2, 4, '2022-01-05'),
+(3, 3, 1, '2022-01-06'),
+(4, 1, 3, '2022-01-07'),
+(4, 2, 2, '2022-01-08'),
+(5, 3, 6, '2022-01-09'),
+(5, 1, 2, '2022-01-10');
+
+	delimiter //
+	CREATE PROCEDURE actualizar_stock(IN p_producto_id INT, IN p_cantidad INT, OUT p_nuevo_stock INT)
+	BEGIN
+		UPDATE productos
+		SET stock = stock + p_cantidad
+		WHERE id = p_producto_id;
+
+		SELECT stock INTO p_nuevo_stock
+		FROM productos
+		WHERE id = p_producto_id;
+	END //
+	delimiter ;
+call actualizar_stock(1, -2, @nuevo_stock);
+
+/*
+5 - Crear una vista que muestre el título, el autor, el precio y la editorial de todos los libros de cocina de la base pubs.
+*/
+use pubs;
+create view vista3 as
+select t.title_id titulo, au.au_fname nombre, t.price precio, pu.pub_name editorial, t.type tipo
+from publishers pu
+join  titles t
+on pu.pub_id = t.pub_id
+join titleauthor tau
+on t.title_id = tau.title_id
+join authors au
+on au.au_id = tau.au_id
+where t.type  like '%cook%';
+select * from vista3;
+
+-- 6   Dadas las siguientes tablas:
+CREATE TABLE fabricantes (
+    id_fabricante INT PRIMARY KEY,
+    nombre_fabricante VARCHAR(255) NOT NULL
+);
+
+INSERT INTO fabricantes (id_fabricante, nombre_fabricante)
+VALUES(1, 'Fabricante A'),(2, 'Fabricante B'),(3, 'Fabricante C');
+
+CREATE TABLE productos (
+    id_producto INT PRIMARY KEY,
+    id_fabricante INT,
+    nombre_producto VARCHAR(255) NOT NULL,
+    fecha_lanzamiento DATE,
+    FOREIGN KEY (id_fabricante) REFERENCES fabricantes(id_fabricante)
+);
+
+INSERT INTO productos (id_producto, id_fabricante, nombre_producto, fecha_lanzamiento)
+VALUES(1, 1, 'Producto X', '2020-01-01'),(2, 2, 'Producto Y', '2019-12-01'), (3, 3, 'Producto Z', '2021-05-15');
+
+create index fabricante_producto	
+on productos (id_fabricante,nombre_producto);
+
+create unique index index_unico	
+on productos(id_producto);
+				
+alter table	productos
+add unique index index_2 (id_fabricante,nombre_producto);
+
+create unique index unico
+on productos(id_fabricante);
+
+drop index index_2
+on productos;
